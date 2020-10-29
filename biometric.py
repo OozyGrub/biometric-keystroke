9369d682e2a5679a1fe2959fb09ddda101a1ad81
oozygrub <oozygrub@hotmail.com>
2020-10-29 07:37:30 +0700

    :wrench: Change output to summation of diff time

--- biometric.py
+++ biometric.py
@@ -3,9 +3,8 @@ from time import time
 
 END_STRING = '\r'
 TEST_WORDS = 'apple ball car doll elephant fish'
-weight = dict()
 
-def gatherData(weight):
+def gatherData():
     print('Please print these words, when finish press enter: \n'+TEST_WORDS+'\n\n')
     
     timeStamp = []
@@ -28,30 +27,20 @@ def gatherData(weight):
         if(table.get(key) == None):
             table[key] = timeStamp[i+1] - timeStamp[i]
             count[key] = 1
-            if(weight != None):
-                weight[key] = 1
         else:
             table[key] += timeStamp[i+1] - timeStamp[i]
             count[key] += 1
-            if(weight != None):
-                weight[key] += 1
 
     for key in table:
         table[key] /= count[key]
-    weight = count
     return table
 
-def verify(template, authen, weight):
+def verify(template, authen):
     count = 0.00
     sum = 0.00
     for key in authen:
         if(template.get(key) != None):
-            # and ' ' not in key and END_STRING not in key
-            # sum += abs(template[key] - authen[key])/template[key] * weight[key] * 100 Per
-            # count += weight[key]
-            # return sum/count
             sum += abs(template[key] - authen[key])
-            
             count += 1
     return sum
     
@@ -59,11 +48,11 @@ def verify(template, authen, weight):
 
 
 print('Making template...\n')
-template = gatherData(None)
+template = gatherData()
 
 print('Authentication...\n')
-authen = gatherData(weight)
+authen = gatherData()
 
 print('Verification...\n')
-diffPercentage = verify(template, authen, weight)
-print('Different Percentage: %.2f'%(diffPercentage))
+diffTime = verify(template, authen)
+print('Summation of diff time: %.2f'%(diffTime))
